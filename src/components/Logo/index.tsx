2bef9bf2628c8acf556980d0fdfe44d82bebe11c
Ryan Izac <io.ryan@outlook.com>
2023-01-04 11:02:14 -0300

    conf: add svgr

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,13 +1,12 @@
 import { Container, Text } from "./styles";
-import NextImage from "next/image";
-import codeSVG from "@public/svg/code.svg";
+import CodeSVG from "@public/svg/code.svg";
 import { Link } from "../Link";
 
 export function Logo() {
   return (
     <Link href="/" legacyBehavior>
       <Container>
-        <NextImage src={codeSVG} alt="Code svg" width={32} height={32} />
+        <CodeSVG width={32} height={32} />
         <Text>Ryan Izac</Text>
       </Container>
     </Link>
